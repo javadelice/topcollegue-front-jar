f2308f1873790bd9a068b3b69519909973286c37
Jean-Baptiste Tâm MATHIEU <jbtmathieu@gmail.com>
2019-07-25 15:18:06 +0200

    #6 Data Service + models User &Vote + backendUrl specified

---
+++ src/app/models/User.ts
@@ -0,0 +1,7 @@
+export class User {
+  constructor(
+    public matricule: string,
+    public nom: string,
+    public prenoms: string,
+    public photoUrl: string) {}
+}
---
+++ src/app/models/Vote.ts
@@ -0,0 +1,6 @@
+export class Vote {
+  constructor(
+    public votant: string,
+    public typeVote: string,
+    public elu: string) {}
+}
