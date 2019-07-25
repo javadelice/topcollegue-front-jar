e21cfa658f75b0090a87f7a1438084b170782e5b
Jean-Baptiste Tâm MATHIEU <jbtmathieu@gmail.com>
2019-07-26 00:02:56 +0200

    refactoring

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, BehaviorSubject } from 'rxjs';
 import { User } from '../models/User';
 
 const URL_BACKEND = environment.backendUrl;
@@ -11,9 +11,18 @@ const URL_BACKEND = environment.backendUrl;
 })
 export class AuthService {
 
+  isLoginSubject = new BehaviorSubject<boolean>(this.hasToken());
+ /**
+   * if we have token the user is loggedIn
+   * 
+   */
+  private hasToken() : boolean {
+    return !!localStorage.getItem('token');
+  }
   constructor(private httpClient: HttpClient) { }
 
   authentifier(nomUtilisateur: string, motDePasse: string, photoUrl: string) {
+    localStorage.setItem('token', 'JWT');
     return this.httpClient.post(URL_BACKEND + 'topcollegues/auth', {
       "nomUtilisateur": nomUtilisateur,
       "motDePasse": motDePasse,
@@ -22,11 +31,13 @@ export class AuthService {
   }
 
   logout() {
-    return this.httpClient.post(URL_BACKEND + '/logout', {}, {withCredentials:true});
+    localStorage.removeItem('token');
+    this.isLoginSubject.next(false);
+    return this.httpClient.post(URL_BACKEND + 'logout', {}, {withCredentials:true});
   }
 
   isLoggedIn(): Observable<User> {
-    return this.httpClient.get<User>(URL_BACKEND + '/topcollegues/me', {withCredentials:true});
+    return this.httpClient.get<User>(URL_BACKEND + 'topcollegues/me', {withCredentials:true});
   }
 
 }
