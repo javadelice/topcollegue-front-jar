44e602f81fe66cdbd3fd8c0913f6a127c102104d
Alexei AYINDE <alexei.ayinde@gmail.com>
2019-07-26 16:54:20 +0200

    issue data.service

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -70,46 +70,6 @@ posterVote(matriVotant: string, operation: string,  matriElu: string): Observabl
 
 }
 
-  constructor(private httpClient: HttpClient) { }
-
-  publierUser(user: User) {
-    return this.userSelectionne.next(user);
-  }
-
-  recupUser(): Observable<User> {
-    return this.userSelectionne.asObservable();
-  }
-
-  recupUserList(): Observable<User[]> {
-    return this.httpClient.get<User[]>(environment.backendUrl + 'PATH_TO_GET_ALL_USER', { withCredentials: true })
-      .pipe(
-        map(tableauUserRecup =>
-          tableauUserRecup.map(oneUser => {
-            return oneUser;
-          }, (error: any) => {
-            // cas erreur
-          })
-        )
-      );
-  }
-
-  recupVoteList(): Observable<Vote[]> {
-    return this.httpClient.get<Vote[]>(environment.backendUrl + 'PATH_TO_GET_ALL_VOTE', { withCredentials: true })
-      .pipe(
-        map(tableauUserRecup =>
-          tableauUserRecup.map(oneUser => {
-            return oneUser;
-          }, (error: any) => {
-            // cas erreur
-          })
-        )
-      );
-  }
-
-  recupScore(): Observable<EluScoreDTO[]> {
-    return this.httpClient.get<EluScoreDTO[]>(environment.backendUrl + 'topcollegues/score', {withCredentials:true});
-  }
-
 }
 
 
