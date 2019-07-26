a432362e6cebefd1ffc47f5d041491aa2fbca0c5
alexeiayinde <51161878+alexeiayinde@users.noreply.github.com>
2019-07-26 16:49:45 +0200

    Update auth.service.ts

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -11,15 +11,6 @@ const URL_BACKEND = environment.backendUrl;
 })
 export class AuthService {
 
-
-  isLoginSubject = new BehaviorSubject<boolean>(this.hasToken());
- /**
-   * if we have token the user is loggedIn
-   *
-   */
-  private hasToken() : boolean {
-    return !!localStorage.getItem('token');
-
   private subLogin = new Subject<boolean>();
   publierLogin(isLoggedIn:boolean) {
     this.subLogin.next(isLoggedIn);
@@ -40,13 +31,6 @@ export class AuthService {
   }
 
   logout() {
-
-    localStorage.removeItem('token');
-    this.isLoginSubject.next(false);
-    localStorage.removeItem('currentUser');
-      localStorage.removeItem('token');
-      sessionStorage.removeItem('token');
-
     return this.httpClient.post(URL_BACKEND + 'logout', {}, {withCredentials:true});
   }
 
